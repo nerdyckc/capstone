c6b34160a4bf6c32159533e9855eb77b2643fa40
nerdyckc <chekitsch@protonmail.com>
2018-08-16 12:46:18 +0800

    kickstarter

---
+++ eth_course/kickstart/ethereum/contracts/Campaign.sol
@@ -0,0 +1,87 @@
+pragma solidity ^0.4.24;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);    // msg is a global variable
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns (address[]) {
+        // view - no data inside the contract is modified
+        // returns (address[]) - returns an array of address
+        
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    // address[] public approvers;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    
+    modifier restricted() {
+        require(msg.sender == manager, "only the manager can invoke this function");
+        _;
+    }
+    
+    constructor (uint minimum, address creator) public {
+        // msg is a global variable
+        manager = creator;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution, "your contribution amount does not meet minimum requirement");
+        
+        // add key value pair to mapping
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        // struct instance creation
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        
+        require(approvers[msg.sender], "you must be a donator to approve a request");   // make sure sender is a donator
+        require(!request.approvals[msg.sender], "Duplicate voting not allowed!!!");
+        
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+    
+    function finalizeRequest (uint index) public restricted {
+        Request storage request = requests[index];
+        
+        require(request.approvalCount > (approversCount / 2), "At least half the contributors must approve");
+        require(!request.complete, "no duplicate finalize");
+        
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
