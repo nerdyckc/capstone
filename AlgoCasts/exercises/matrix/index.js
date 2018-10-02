22bff531022dced9079bc739709249454090354c
nerdyckc <chekitsch@protonmail.com>
2018-10-02 11:05:25 +0800

    matrix

--- AlgoCasts/exercises/matrix/index.js
+++ AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,45 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let result = [];
+
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  let square = 1;
+
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let i = startCol; i <= endCol; i++) {
+      result[startRow][i] = square;
+      square += 1;
+    }
+    startRow += 1;
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endCol] = square;
+      square += 1;
+    }
+    endCol -= 1;
+    for (let i = endCol; i >= startCol; i--) {
+      result[endRow][i] = square;
+      square += 1;
+    }
+    endRow -= 1;
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startCol] = square;
+      square += 1;
+    }
+    startCol++;
+    console.log('startRow', startRow,'endRow', endRow,'startCol', startCol,'endCol', endCol);
+    // console.log(result);
+  }
+
+  return result;
+}
 
 module.exports = matrix;
