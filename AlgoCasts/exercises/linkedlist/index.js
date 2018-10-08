8d416535d3c5358a69dff606818417a3d5770e6e
nerdyckc <chekitsch@protonmail.com>
2018-10-08 15:59:38 +0800

    linked list in progress

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -2,8 +2,33 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let count = 0;
+    let currNode = this.head;
+    
+    while (currNode) {
+      // console.log('current node: ', currNode);
+      count++;
+      currNode = currNode.next;
+    }
+    return count;
+  }
+}
 
 module.exports = { Node, LinkedList };
