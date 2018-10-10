76998a686cd7f54d538111df333bdfda081b4447
nerdyckc <chekitsch@protonmail.com>
2018-10-10 19:19:56 +0800

    getAt done

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -21,7 +21,25 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data, this.head);
   }
+  insertLast(data) {
+    const lastNode = this.getLast();
 
+    if (lastNode) {
+      lastNode.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+  getAt(index) {
+    let currNode = this.head;
+    let counter = 0;
+
+    while (currNode && counter < index) {
+      counter++;
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
   getFirst() {
     return this.head;
   }
@@ -29,18 +47,41 @@ class LinkedList {
   getLast() {
     let currNode = this.head;
 
-    while(currNode) {
+    while (currNode) {
       if (!currNode.next) {
         return currNode;
       }
       currNode = currNode.next;
     }
   }
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
 
+    this.head = this.head.next;
+  }
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let prev = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      prev = prev.next;
+      node = node.next;
+    }
+    prev.next = null;
+  }
   size() {
     let count = 0;
     let currNode = this.head;
-    
+
     while (currNode) {
       // console.log('current node: ', currNode);
       count++;
